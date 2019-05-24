5b91e09be34e6002a472780e7fd3f40680f1ff86
Rats <juliocesarmmc@hotmail.com>
2019-05-24 07:09:55 -0300

    EVENTO CHEGADA DE EVENTOS IMPLEMENTADO

--- src/include/atividade.h
+++ src/include/atividade.h
@@ -1,37 +1,45 @@
 #ifndef ACT
 #define  ACT
 
+#include "vaso.h"
+#include "especialista.h"
+#include "artesao.h"
+#include "triangular.h"
+#include "simulacao.h"
+#include "TAD.h"
+#include "fila.h"
+
 // SET == ACABAMENTO CARALHO.
 
 
-void begin_requets();
-
+void begin_requets();       // EVENTO := 'CHEGADA DE PEDIDOS'
+//
 void base_preparation();
-
-void base_set_init();
-
-void base_set_drying();
-
-void base_clearing();
-
-void base_finish();
-
-void mouth_preparation();
-
-void mouth_set_init();
-
-void mouth_set_drying();
-
-void mount_clearing();
-
-void mount_drying();
-
-void inter_waterpoofing();
-
-void inter_drying();
-
-void varnishing();
-
-void final_drying();
+//
+// void base_set_init();
+//
+// void base_set_drying();
+//
+// void base_clearing();
+//
+// void base_finish();
+//
+// void mouth_preparation();
+//
+// void mouth_set_init();
+//
+// void mouth_set_drying();
+//
+// void mount_clearing();
+//
+// void mount_drying();
+//
+// void inter_waterpoofing();
+//
+// void inter_drying();
+//
+// void varnishing();
+//
+// void final_drying();
 
 #endif
