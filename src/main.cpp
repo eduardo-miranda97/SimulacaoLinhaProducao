94e48b71f6225f01910c83539b4ab107aa0de50a
Rats <juliocesarmmc@hotmail.com>
2019-05-24 16:57:22 -0300

    testes

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,8 @@
 int main(int argc, char* argv[]){
     srand(time(NULL));
     SM_queue_vasos = (Vaso***) malloc(sizeof(Vaso**)*QUEUE_WAIT);
+    SM_time_simulation = 0;
+    SM_final_time_simulation = 72*24*60;
     /*
     for (int i=0; i<QUEUE_WAIT;i++){
       Vaso* v1 = new Vaso(i, SMALL, i);
@@ -19,7 +21,8 @@ int main(int argc, char* argv[]){
     }
     */
 
-    begin_requets();
+    init_simulation();
+    begin_requets(NULL);
 
     for (int i=0; i<SM_length_queue_vasos[PREPARA_BASE];i++){
       std::cout << "ID:\t" << SM_queue_vasos[PREPARA_BASE][i]->get_id() << "\tTIME:\t" <<  SM_queue_vasos[PREPARA_BASE][i]->get_queue(PREPARA_BASE) << '\n';
