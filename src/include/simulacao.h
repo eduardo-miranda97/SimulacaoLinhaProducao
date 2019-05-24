d4cb9413a9ea4072cb83ad3c06aa1f783e62e915
Rats <juliocesarmmc@hotmail.com>
2019-05-24 07:10:48 -0300

    INICIO DO SIMULADOR, AINDA EM IMPLEMENTAÇÃO

---
+++ src/include/simulacao.h
@@ -0,0 +1,11 @@
+#ifndef SIMULACAO
+#define  SIMULACAO
+
+#include "triangular.h"
+
+extern double               SM_time_simulation;
+extern u_int16_t            SM_massa;
+extern u_int16_t            SM_espaco_secagem;
+extern times_triangular_t   SM_quatd_vasos;
+
+#endif
---
+++ src/simulacao.cpp
@@ -0,0 +1,6 @@
+#include "./include/simulacao.h"
+
+double               SM_time_simulation = 0;
+u_int16_t            SM_massa = 0;
+u_int16_t            SM_espaco_secagem = 0;
+times_triangular_t   SM_quatd_vasos = {0, 5, 10};
