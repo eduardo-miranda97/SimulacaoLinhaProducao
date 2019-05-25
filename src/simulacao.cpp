82e98945c9bf8ea9a1121c2ba9b007c94b318504
Rats <juliocesarmmc@hotmail.com>
2019-05-25 10:15:39 -0300

    dados pra testes

--- src/simulacao.cpp
+++ src/simulacao.cpp
@@ -2,6 +2,6 @@
 
 double               SM_final_time_simulation = 60;
 double               SM_time_simulation      = 0;
-u_int16_t            SM_massa                = 0;
-u_int16_t            SM_espaco_secagem       = 0;
-times_triangular_t   SM_quatd_vasos          = {0, 5, 10};
+u_int16_t            SM_massa                = 10;
+u_int16_t            SM_espaco_secagem       = 10;
+times_triangular_t   SM_quatd_vasos          = {1, 5, 10};
