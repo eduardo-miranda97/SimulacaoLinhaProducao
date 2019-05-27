d9c1ebb6991cc52ed536f7b7ed8015bbf54f4236
Rats <juliocesarmmc@hotmail.com>
2019-05-27 19:45:26 -0300

    dados para testes

--- src/include/simulacao.h
+++ src/include/simulacao.h
@@ -1,7 +1,9 @@
 #ifndef SIMULACAO
 #define  SIMULACAO
 
+#include <iostream>
 #include "triangular.h"
+#include "vaso.h"
 
 extern double               SM_time_simulation;
 extern double               SM_final_time_simulation;
@@ -9,5 +11,7 @@ extern u_int16_t            SM_massa;
 extern u_int16_t            SM_pedra;
 extern u_int16_t            SM_espaco_secagem;
 extern times_triangular_t   SM_quatd_vasos;
+extern Vaso**               SM_vaso_finish;
+extern u_int16_t            SM_vaso_finish_length;
 
 #endif
--- src/simulacao.cpp
+++ src/simulacao.cpp
@@ -5,4 +5,6 @@ double               SM_time_simulation      = 0;
 u_int16_t            SM_massa                = 300;
 u_int16_t            SM_pedra                = 300;
 u_int16_t            SM_espaco_secagem       = 300;
-times_triangular_t   SM_quatd_vasos          = {40, 50, 60};
+u_int16_t            SM_vaso_finish_length    = 0;
+times_triangular_t   SM_quatd_vasos          = {100, 150, 200};
+Vaso**               SM_vaso_finish           = NULL;
