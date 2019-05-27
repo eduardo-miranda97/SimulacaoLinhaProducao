45e02f3b622845c3d8feaa3517f82d3b3efed1c3
Rats <juliocesarmmc@hotmail.com>
2019-05-27 15:35:36 -0300

    dados de teste

--- src/simulacao.cpp
+++ src/simulacao.cpp
@@ -5,4 +5,4 @@ double               SM_time_simulation      = 0;
 u_int16_t            SM_massa                = 300;
 u_int16_t            SM_pedra                = 300;
 u_int16_t            SM_espaco_secagem       = 300;
-times_triangular_t   SM_quatd_vasos          = {1, 5, 10};
+times_triangular_t   SM_quatd_vasos          = {40, 50, 60};
