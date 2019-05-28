c00bb45413639e930d1653856a73ddaaefb13cb0
Rats <juliocesarmmc@hotmail.com>
2019-05-28 00:52:12 -0300

    var para testes

--- src/include/simulacao.h
+++ src/include/simulacao.h
@@ -2,16 +2,20 @@
 #define  SIMULACAO
 
 #include <iostream>
+#include <list>
 #include "triangular.h"
 #include "vaso.h"
 
 extern double               SM_time_simulation;
 extern double               SM_final_time_simulation;
 extern u_int16_t            SM_massa;
+extern u_int16_t            SM_massa_MAX;
 extern u_int16_t            SM_pedra;
+extern u_int16_t            SM_pedra_MAX;
 extern u_int16_t            SM_espaco_secagem;
+// extern u_int16_t            SM_vaso_finish_length;
+// extern Vaso**               SM_vaso_finish;
+extern std::list<Vaso*>            SM_vaso_finish;
 extern times_triangular_t   SM_quatd_vasos;
-extern Vaso**               SM_vaso_finish;
-extern u_int16_t            SM_vaso_finish_length;
 
 #endif
--- src/simulacao.cpp
+++ src/simulacao.cpp
@@ -1,10 +1,13 @@
 #include "./include/simulacao.h"
 
-double               SM_final_time_simulation = 60;
+double               SM_final_time_simulation = 0;
 double               SM_time_simulation      = 0;
 u_int16_t            SM_massa                = 300;
+u_int16_t            SM_massa_MAX            = 300;
 u_int16_t            SM_pedra                = 300;
+u_int16_t            SM_pedra_MAX            = 300;
 u_int16_t            SM_espaco_secagem       = 300;
 u_int16_t            SM_vaso_finish_length    = 0;
-times_triangular_t   SM_quatd_vasos          = {100, 150, 200};
-Vaso**               SM_vaso_finish           = NULL;
+times_triangular_t   SM_quatd_vasos          = {300, 350, 400};
+// Vaso**               SM_vaso_finish           = NULL;
+std::list<Vaso*>     SM_vaso_finish;
