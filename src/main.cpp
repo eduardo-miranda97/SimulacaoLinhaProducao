ee102187bbe165d27c9425e71b096cad29e2c997
Rats <juliocesarmmc@hotmail.com>
2019-06-06 01:03:22 -0300

    main sendo utilizado para testes

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
-#include <time.h>
 #include "./include/atividade.h"
+#include "./include/input.h"
+#include <time.h>
 #include <list>
 #include <vector>
 #include <string>
@@ -11,37 +12,52 @@ int main(int argc, char* argv[]){
     srand(time(NULL));
 
     SM_time_simulation = 0;
-    SM_final_time_simulation = 78*24*60;
+    SM_final_time_simulation = 150*24*60;
 
     Artesao a0;
     Artesao a1;
     Artesao a2;
     Artesao a3;
+    Artesao a4;
+    Artesao a5;
 
     a0.add_art();
     a1.add_art();
     a2.add_art();
     a3.add_art();
+    a4.add_art();
+    a5.add_art();
 
     Especialista e0;
     Especialista e1;
     Especialista e2;
     Especialista e3;
+    Especialista e4;
+    Especialista e5;
 
     e0.add_esp();
     e1.add_esp();
     e2.add_esp();
     e3.add_esp();
+    e4.add_esp();
+    e5.add_esp();
 
+    input_args("config.cfg");
     init_simulation();
-    while(SM_list_event_simulation){
-        event_list_t act = SM_list_event_simulation[0];
-        act.event.funct_event();
+    while ((SM_list_event_simulation)&&(SM_time_simulation <= SM_final_time_simulation)){
+        SM_list_event_simulation[0].event.funct_event();
     }
 
-    for (Vaso* vaso: SM_vaso_finish){
-        printf("Vaso ID: %ld\n", vaso->get_id());
+    for(int i=0;i<QUEUE_WAIT;i++){
+        std::cout << "FILA ["<< i << "] = " << SM_queue_vasos[i].size() << '\n';
     }
-    printf("\nUltimo ID vaso: %ld\n", last_id_vaso-1);
+
+    std::cout << '\n';
+
+    std::cout << "Time atual: " << SM_time_simulation << '\n';
+    std::cout << "Time Final: " << SM_final_time_simulation << '\n';
+    std::cout << "Vasos Gerados: " << last_id_vaso << '\n';
+    std::cout << "Vasos Finalizados: " << SM_vaso_finish.size() << '\n';
+    printf("Massa: %ld, Pedra: %ld, Espaço: %ld\n", SM_massa, SM_pedra, SM_espaco_secagem);
     return 0;
 }
