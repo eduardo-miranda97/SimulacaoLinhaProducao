25b4e1eda64a573f44e5e294a6a3223933d4ef4e
Rats <juliocesarmmc@hotmail.com>
2019-06-08 15:27:44 -0300

    corpo inicial da inicialização da simulação

--- src/include/simulacao.h
+++ src/include/simulacao.h
@@ -1,25 +1,23 @@
 #ifndef SIMULACAO
 #define  SIMULACAO
 
-#include <iostream>
-#include <cstdlib>
-#include <list>
 #include <map>
-#include "triangular.h"
-#include "vaso.h"
+#include "atividade.h"
 
 extern std::map<std::string, times_triangular_t> SM_times_events;
-extern double               SM_time_simulation;
-extern double               SM_final_time_simulation;
-extern u_int16_t            SM_massa;
-extern u_int16_t            SM_massa_MAX;
-extern u_int16_t            SM_pedra;
-extern u_int16_t            SM_pedra_MAX;
-extern u_int16_t            SM_espaco_secagem;
-extern std::list<Vaso*>     SM_vaso_finish;
-extern times_triangular_t   SM_quatd_vasos;
-extern u_int16_t            SM_quatd_art;
-extern times_triangular_t   SM_quatd_pedidos;
-extern u_int16_t            SM_quatd_esp;
+extern std::list<Vaso*>                          SM_vaso_finish;
+extern double                                    SM_time_simulation;
+extern double                                    SM_final_time_simulation;
+extern u_int16_t                                 SM_massa;
+extern u_int16_t                                 SM_massa_MAX;
+extern u_int16_t                                 SM_pedra;
+extern u_int16_t                                 SM_pedra_MAX;
+extern u_int16_t                                 SM_espaco_secagem;
+extern u_int16_t                                 SM_quatd_art;
+extern u_int16_t                                 SM_quatd_esp;
+extern times_triangular_t                        SM_quatd_pedidos;
+extern times_triangular_t                        SM_quatd_vasos;
+
+void start_simulation();
 
 #endif
--- src/simulacao.cpp
+++ src/simulacao.cpp
@@ -2,14 +2,34 @@
 
 double               SM_final_time_simulation = 0;
 double               SM_time_simulation      = 0;
-u_int16_t            SM_massa                = 30;
-u_int16_t            SM_massa_MAX            = 300;
-u_int16_t            SM_pedra                = 30;
-u_int16_t            SM_pedra_MAX            = 300;
-u_int16_t            SM_espaco_secagem       = 80;
+u_int16_t            SM_quatd_esp            = 2;
+u_int16_t            SM_quatd_art            = 2;
+u_int16_t            SM_massa                = 10;
+u_int16_t            SM_massa_MAX            = 10;
+u_int16_t            SM_pedra                = 10;
+u_int16_t            SM_pedra_MAX            = 10;
+u_int16_t            SM_espaco_secagem       = 10;
 times_triangular_t   SM_quatd_vasos          = {20, 40, 60};
 times_triangular_t   SM_quatd_pedidos        = {21, 30, 37};
-u_int16_t            SM_quatd_esp            = 1;
-u_int16_t            SM_quatd_art            = 1;
-std::list<Vaso*>     SM_vaso_finish;
-std::map<std::string, times_triangular_t> SM_times_events;
+std::map<std::string, times_triangular_t>    SM_times_events;
+std::list<Vaso*>                             SM_vaso_finish;
+
+using namespace std;
+
+void start_simulation(){
+
+  for (u_int16_t i = 0; i < SM_quatd_art; i++) {
+      Artesao artesao;
+      artesao.add_art();
+  }
+
+  for (u_int16_t i = 0; i < SM_quatd_esp; i++) {
+      Especialista especialista;
+      especialista.add_esp();
+  }
+
+  init_simulation();
+  while ((SM_list_event_simulation)&&(SM_time_simulation <= SM_final_time_simulation)){
+      SM_list_event_simulation[0].event.funct_event();
+  }
+}
