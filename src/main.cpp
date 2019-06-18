b4e6eaa2c32381530bd236fef509f06260962172
Rats <juliocesarmmc@hotmail.com>
2019-06-18 17:52:36 -0300

    nova saida de resultados

--- src/main.cpp
+++ src/main.cpp
@@ -4,43 +4,92 @@
 using namespace std;
 
 int main(int argc, char* argv[]){
-    srand(time(NULL));
-
-    if (argc != 2){
+    if (argc != 4){
         printf("\nErro parametro, usar segundo sintaxe:\n\n");
-        printf(" %s [input file]\n\n", argv[0]);
+        printf(" %s [input file] [result file] [seed rand]\n\n", argv[0]);
         exit(ERRO_INPUT_FILE_ARGS);
     }
 
+    srand(stoul(argv[3]));
     input_args(argv[1]);
     start_simulation();
 
-    std::cout << '\n';
+    if (0){
+        std::cout << '\n';
 
-    for (int i=0; i < QUEUE_WAIT; i++){
-        printf("Fila [%d] = %ld\n", i, SM_queue_vasos[i].size());
+        for (int i=0; i < QUEUE_WAIT; i++){
+            printf("Fila [%d] = %ld\n", i, SM_queue_vasos[i].size());
+        }
+
+        std::cout << '\n';
+
+        for (Artesao* artesao: SM_Artesao) {
+            printf("Artesao ID: %4u \tociosidade: %5ld\n", artesao->get_id(), artesao->get_time_ociosity());
+        }
+
+        std::cout << '\n';
+
+        for (Especialista* especialista: SM_Especialista) {
+            printf("Especialista ID: %4u\t  ociosidade: %5ld\n", especialista->get_id(), especialista->get_time_ociosity());
+        }
+
+        std::cout << '\n';
+
+        for (Vaso* vaso: SM_vaso_finish){
+            printf("Vaso ID: %4u\tTempo total: %5u\n", vaso->get_id(), vaso->get_end_time()-vaso->get_start_time());
+        }
+
+        std::cout << '\n';
+
+        std::cout << "Time Final:\t"        << SM_final_time_simulation << '\n';
+        std::cout << "Time atual:\t"        << SM_time_simulation      << '\n';
+        std::cout << '\n';
+        std::cout << "Vasos Gerados:  \t"   << last_id_vaso            << '\n';
+        std::cout << "Vasos Finalizados:\t" << SM_vaso_finish.size()    << '\n';
+        std::cout << '\n';
+        printf("Massa: %u, Pedra: %u, Espaço: %u\n\n", SM_massa, SM_pedra, SM_espaco_secagem);
+    }
+    double ociosidade_art = 0;
+    double ociosidade_esp = 0;
+    double produt_art     = SM_time_simulation*SM_quatd_art;
+    double produt_esp     = SM_time_simulation*SM_quatd_esp;
+
+    for (Artesao* artesao: SM_Artesao){
+        ociosidade_art += artesao->get_time_ociosity();
     }
 
-    std::cout << '\n';
-
-    for (Artesao* artesao: SM_Artesao) {
-        printf("Artesao ID: %d, ociosidade: %ld\n", artesao->get_id(), artesao->get_time_ociosity());
+    for (Especialista* especialista: SM_Especialista){
+        ociosidade_esp += especialista->get_time_ociosity();
     }
 
-    std::cout << '\n';
+    produt_art -= ociosidade_art;
+    produt_esp -= ociosidade_esp;
 
-    for (Especialista* especialista: SM_Especialista) {
-        printf("Especialista ID: %d, ociosidade: %ld\n", especialista->get_id(), especialista->get_time_ociosity());
+    FILE* result_file = fopen(argv[2], "r+");
+
+    if (!result_file){
+        result_file = fopen(argv[2], "w+");
+        if (!result_file){
+          printf("\n================================================================================\n");
+          printf("\n[ERRO] Ao tentar abrir arquivo %s em main()\n", argv[2]);
+          printf("\n================================================================================\n");
+          exit(ERRO_INPUT_FILE);
+        }
+        fprintf(result_file, "TEMPO_TRABALHO\tUSO_MASSA\tUSO_PEDRA\tPROD_ART\tPROD_ESP\tOCIOSID_ART\tOCIOSID_ESP\tSEED\n");
+    }else{
+        result_file = fopen(argv[2], "a+");
     }
 
-    std::cout << '\n';
+    if (!result_file){
+        printf("\n================================================================================\n");
+        printf("\n[ERRO] Ao tentar abrir arquivo %s em main()\n", argv[2]);
+        printf("\n================================================================================\n");
+        exit(ERRO_INPUT_FILE);
+    }
 
-    std::cout << "Time Final: "        << SM_final_time_simulation << '\n';
-    std::cout << "Time atual: "        << SM_time_simulation      << '\n';
-    std::cout << '\n';
-    std::cout << "Vasos Gerados: "     << last_id_vaso            << '\n';
-    std::cout << "Vasos Finalizados: " << SM_vaso_finish.size()    << '\n';
-    std::cout << '\n';
-    printf("Massa: %d, Pedra: %d, Espaço: %d\n\n", SM_massa, SM_pedra, SM_espaco_secagem);
+    fprintf(result_file, "%.2lf\t%lu\t%lu\t%.2lf\t%.2lf\t%.2lf\t%.2lf\t%d\n", SM_time_simulation, SM_massa_usado, SM_pedra_usado, produt_art,
+                        produt_esp, ociosidade_art, ociosidade_esp, stoul(argv[3]));
+
+    fclose(result_file);
     return 0;
 }
