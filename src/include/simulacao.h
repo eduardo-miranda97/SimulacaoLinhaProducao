8bcd47511f4790b75699b28668f49a0b02d1404a
Rats <juliocesarmmc@hotmail.com>
2019-06-18 17:52:18 -0300

    variaveis Default

--- src/include/simulacao.h
+++ src/include/simulacao.h
@@ -2,16 +2,22 @@
 #define  SIMULACAO
 
 #include <map>
+#include <fstream>
 #include "atividade.h"
+
+#define LOG  false
 
 extern std::map<std::string, times_triangular_t> SM_times_events;
 extern std::list<Vaso*>                          SM_vaso_finish;
+extern std::ofstream                             file_log;
 extern double                                    SM_time_simulation;
 extern double                                    SM_final_time_simulation;
 extern u_int16_t                                 SM_massa;
 extern u_int16_t                                 SM_massa_MAX;
+extern u_int16_t                                 SM_massa_usado;
 extern u_int16_t                                 SM_pedra;
 extern u_int16_t                                 SM_pedra_MAX;
+extern u_int16_t                                 SM_pedra_usado;
 extern u_int16_t                                 SM_espaco_secagem;
 extern u_int16_t                                 SM_quatd_art;
 extern u_int16_t                                 SM_quatd_esp;
--- src/simulacao.cpp
+++ src/simulacao.cpp
@@ -1,18 +1,21 @@
 #include "./include/simulacao.h"
 
-double               SM_final_time_simulation = 0;
-double               SM_time_simulation      = 0;
-u_int16_t            SM_quatd_esp            = 2;
-u_int16_t            SM_quatd_art            = 2;
+double               SM_final_time_simulation =  0;
+double               SM_time_simulation      =  0;
+u_int16_t            SM_quatd_esp            =  2;
+u_int16_t            SM_quatd_art            =  2;
 u_int16_t            SM_massa                = 10;
 u_int16_t            SM_massa_MAX            = 10;
 u_int16_t            SM_pedra                = 10;
 u_int16_t            SM_pedra_MAX            = 10;
 u_int16_t            SM_espaco_secagem       = 10;
+u_int16_t            SM_pedra_usado          =  0;
+u_int16_t            SM_massa_usado          =  0;
 times_triangular_t   SM_quatd_vasos          = {20, 40, 60};
 times_triangular_t   SM_quatd_pedidos        = {21, 30, 37};
 std::map<std::string, times_triangular_t>    SM_times_events;
 std::list<Vaso*>                             SM_vaso_finish;
+std::ofstream                                file_log;
 
 using namespace std;
 
@@ -32,6 +35,10 @@ void start_simulation(){
   while ((SM_list_event_simulation)&&(SM_time_simulation <= SM_final_time_simulation)){
       SM_list_event_simulation[0].event.funct_event();
   }
+
+  #if LOG
+      file_log.close();
+  #endif
 
   for (Artesao* artesao: SM_Artesao){
       if (artesao->get_situation()==state_art::OCIOSITY_ART)
